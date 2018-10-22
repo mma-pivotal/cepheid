7b7bc06f2223d0440ee3979b0f32d13cad7192ba
mma-pivotal <mma@pivotal.io>
2018-10-22 14:31:06 +0900

    Added IOPS challenge and explaination for switch_timestamp.sh in to readme

--- switch_timestamp.sh
+++ switch_timestamp.sh
@@ -6,6 +6,7 @@ outfile_name="$file_name.out"
 
 while read -r p; do
 	time=$(echo $p | cut -f 1 -d "," | cut -f 2 -d ":" | cut -f 1 -d "." | xargs date -u -r)
+	#currently only works in MacOS, for ubuntu the command should be date -d @timestamp, also it's better to add support for milliseconds as well
 	time="\"$time\""
 	echo $p | sed -E 's|([0-9]{10})\.[0-9]+|'"$time"'|g' >> $outfile_name
 done < $file_name
